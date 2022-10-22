4e64a5eff8889503a39551148b8c8c9d86ad0e80
Nguyễn Việt Kim <kimnguyenblc@gmail.com>
2022-10-22 11:40:01 +0700

    git add CPEOPLE.cpp git add CPEOPLE.h

--- CrossingGame/CPEOPLE.h
+++ CrossingGame/CPEOPLE.h
@@ -4,17 +4,26 @@
 #include "CVEHICLE.h"
 
 class CPEOPLE {
-	int mX, mY;
-	bool mState;		// dead or alive state
+private:
+	int mX = 36, mY = 24;   //Position of character
+	bool mState = true;		// dead or alive state
+	int level = 0, score = 0;
 public:
 	CPEOPLE() {};
 	~CPEOPLE() {};
-	void Up(int);
-	void Left(int);
-	void Right(int);
-	void Down(int);
-	bool isImpact(const CVEHICLE*&);
-	bool isImpact(const CANIMAL*&);
-	bool isFinish();
-	bool isDead();
+	int getPosX();     //Get mX value of character
+	int getPosY();     //Get mY value of character
+	void updatePos(int x, int y);  //Update position of character after move
+	bool isDead();     //Return if character is dead or not
+	void DRAW_PEOPLE(int mX, int mY);  //Draw player's character to screen
+	void Delete(int mX, int mY); //Delete character at previous position
+	void Up(int& mY);            //Move character up
+	void Left(int& mX);          //Move character left
+	void Right(int& mX);         //Move character right
+	void Down(int& mY);          //Move character down
+	bool isImpact(const CVEHICLE*& vehicle); //Check if player impact with vehicle
+	bool isImpact(const CANIMAL*& animal);   //Check if player impact with animal
+	bool isFinish(int mX);             //Check if player has completed the stage
+	void sound(int s);                //Play character's sound effects
+	static void mainPeople();    //Main function of character
 };
